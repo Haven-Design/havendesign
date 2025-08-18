bafb723d843538c9edb17a3797eb238f421db36c
Chris <havendesigndev@gmail.com>
2025-08-18 08:17:38 -0400

    File path update

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import tempfile
 import base64
 import streamlit as st
 import streamlit.components.v1 as components
-from app.utilities.extract_text import extract_text_and_positions
-from app.utilities.redact_pdf import redact_pdf_with_positions
+from utilities.extract_text import extract_text_and_positions
+from utilities.redact_pdf import redact_pdf_with_positions
 
 # Temporary directory for previews
 temp_dir = tempfile.mkdtemp()
